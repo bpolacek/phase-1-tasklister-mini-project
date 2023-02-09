84fa503979dd090ba647a229d85b6dbcbac99c81
Brett Polacek <brettpolacek@gmail.com>
2023-02-09 13:57:58 -0500

    added index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const form = document.getElementById("create-task-form")
+const descriptionField = document.getElementById("new-task-description")
+const taskList = document.getElementById("tasks")
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const task = document.createElement("li")
+  task.textContent = descriptionField.value
+  taskList.append(task)
+  e.target.reset()
+})
